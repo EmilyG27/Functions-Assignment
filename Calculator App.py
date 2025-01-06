6306b415f64df8857c18a00f0265b9878e27cbce
EmilyG27 <133466995+EmilyG27@users.noreply.github.com>
2025-01-05 22:34:47 -0600

    Add files via upload

--- Calculator App.py
+++ Calculator App.py
@@ -1,20 +1,43 @@
 #Task 1
+def add():
+    num_1 = float(input("Enter the first number: "))
+    num_2 = float(input("Enter the second number: "))
+    return num_1 + num_2
 
-while True:
-    try:
-        number_1 = float(input("What is the first number?: "))
-        number_2 = float(input("What is the second number?: "))
-        operator = input("How would you like to calculate your numbers (+, -, *, /)? ")
+def sub():
+    num_1 = float(input("Enter the first number: "))
+    num_2 = float(input("Enter the second number: "))
+    return num_1 - num_2
 
-        if operator == "+":
-            print(number_1 + number_2)
-        elif operator == "-":
-            print(number_1 - number_2)
-        elif operator == "*":
-            print(number_1 * number_2)
-        elif operator == "/":
-            print(number_1 / number_2)
-        else:
-            print("Unknown operation. Please choose again")
-    except ZeroDivisionError:
-        print("Error")
+def mul():
+    num_1 = float(input("Enter the first number: "))
+    num_2 = float(input("Enter the second number: "))
+    return num_1 * num_2
+
+def div():
+    num_1 = float(input("Enter the first number: "))
+    num_2 = float(input("Enter the second number: "))
+    return num_1 / num_2
+
+
+
+def main():
+    while True:
+        try:
+            operator = input("How would you like to calculate your numbers (+, -, *, /)? ")
+
+            if operator == "+":
+                print(add())
+            elif operator == "-":
+                print(sub())
+            elif operator == "*":
+                print(mul())
+            elif operator == "/":
+                print(div())
+            else:
+                print("Unknown operation. Please choose again")
+        except ZeroDivisionError:
+            print("Error")
+
+if __name__ == "__main__":
+    main()
